0f1cc7470d27aa06b26274c1d7bda74c3697edd8
David Blas Vázquez <david.bv@gmail.com>
2017-10-10 22:05:41 +0200

    Added entities Sport and User.

---
+++ src/main/java/es/upm/miw/apaw/user/api/entities/Sport.java
@@ -0,0 +1,32 @@
+package es.upm.miw.apaw.user.api.entities;
+
+public class Sport {
+	private Long id;
+    private String title;
+	private String category;
+	public Sport (Long id) {
+		assert id != null;
+		this.id = id;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		assert id != null;
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		assert title != null;
+		this.title = title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		assert category != null;
+		this.category = category;
+	}
+}
