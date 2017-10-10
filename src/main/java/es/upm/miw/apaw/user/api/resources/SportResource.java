c4e203bb7ad899c31110abb4a52d774bd08d3944
David Blas Vázquez <david.bv@gmail.com>
2017-10-10 08:04:09 +0200

    Created SportsResources's constants  which are used in the functional Testing.

---
+++ src/main/java/es/upm/miw/apaw/user/api/resources/SportResource.java
@@ -0,0 +1,12 @@
+package es.upm.miw.apaw.user.api.resources;
+
+
+public class SportResource {
+
+    public static final String THEMES = "sport";
+
+    public static final String ID = "/{id}";
+
+
+   
+}
