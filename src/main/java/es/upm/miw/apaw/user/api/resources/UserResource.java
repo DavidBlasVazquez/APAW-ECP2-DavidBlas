776e7919cfded839d1a2d3ab724b91bdb2192460
David Blas Vázquez <david.bv@gmail.com>
2017-10-10 08:14:28 +0200

    Created SportsResources's constants and UserResource's constants which are used in the functional testing file.

---
+++ src/main/java/es/upm/miw/apaw/user/api/resources/UserResource.java
@@ -0,0 +1,7 @@
+package es.upm.miw.apaw.user.api.resources;
+
+public class UserResource {
+    public static final String USER = "sport";
+    public static final String DEACTIVE = "deactivate";
+    public static final String ID = "/{id}";
+}
