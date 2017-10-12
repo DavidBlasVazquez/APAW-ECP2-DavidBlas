098addb1019f77c0a33b0baa0416bd3730c714ae
David Blas Vázquez <david.bv@gmail.com>
2017-10-12 17:16:59 +0200

    Create a simple SportDto to pass testing.

---
+++ src/main/java/es/upm/miw/apaw/user/api/dtos/SportDto.java
@@ -0,0 +1,12 @@
+package es.upm.miw.apaw.user.api.dtos;
+
+public class SportDto {
+	private String data;
+	
+	public SportDto (String data) {
+		this.data = data;
+	}
+	public String toString() {
+		return data;	
+	}
+}
