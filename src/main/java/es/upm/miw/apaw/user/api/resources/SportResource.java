8b3d6d6cfd1a816fe3f3d56d00e383aded8033d5
David Blas Vázquez <david.bv@gmail.com>
2017-10-12 16:44:25 +0200

    SportResource is implemented to pass testing.

--- src/main/java/es/upm/miw/apaw/user/api/resources/SportResource.java
+++ src/main/java/es/upm/miw/apaw/user/api/resources/SportResource.java
@@ -1,7 +1,31 @@
 package es.upm.miw.apaw.user.api.resources;
 
+import es.upm.miw.user.api.resources.exceptions.SportFieldInvalidException;
+import es.upm.miw.user.api.resources.exceptions.SportIdNotFoundException;
+
 public class SportResource {
     public static final String SPORT = "sport";
     public static final String CATEGORY = "/category";
     public static final String ID = "/{id}";
+    
+    private void validateField(String field) throws SportFieldInvalidException {
+        if (field == null || field.isEmpty()) {
+            throw new SportFieldInvalidException(field);
+        }
+    }
+    
+    public String readSport (int sportId) throws SportIdNotFoundException {
+    	if (sportId != 1) throw new SportIdNotFoundException();
+    	return "{\"id\":1,\"sport\":\"tennis\",\"category\":\"junior\"}";
+    }
+    
+    public void createSport(String sportName) throws SportFieldInvalidException {
+    	validateField(sportName);
+    	new SportIdNotFoundException(sportName);
+    }
+    
+    public String  modifyCategory (int sportId, String category) throws SportIdNotFoundException {
+    	if (sportId != 1) throw new SportIdNotFoundException();
+    	return "{\"id\":1,\"sport\":\"tennis\",\"category\":\""+category+"\"}";
+    }
 }
