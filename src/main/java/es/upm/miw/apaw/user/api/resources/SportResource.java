f72cad387a3627323bed8cfe8ae510a734affeae
David Blas Vázquez <david.bv@gmail.com>
2017-10-12 05:55:06 +0200

    New methods are added to Sports Resource.

--- src/main/java/es/upm/miw/apaw/user/api/resources/SportResource.java
+++ src/main/java/es/upm/miw/apaw/user/api/resources/SportResource.java
@@ -1,8 +1,29 @@
 package es.upm.miw.apaw.user.api.resources;
 
+import es.upm.miw.user.theme.api.resources.exceptions.SportFieldInvalidException;
+import es.upm.miw.user.theme.api.resources.exceptions.SportIdNotFoundException;
+
 public class SportResource {
     public static final String SPORT = "/sport";
     public static final String CATEGORY = "/category";
     public static final String ID = "/{id}";
-   
+    
+    
+    public String readSport (int sportId) throws SportIdNotFoundException {
+    	return "{\"id\":1,\"sport\":\"tennis\"}";
+    }
+    
+    public void createSport(String sportName) throws SportFieldInvalidException {
+    	validateField(sportName);
+    	new SportIdNotFoundException(sportName);
+    }
+    
+    public void modifyCategory (int sportId) throws SportIdNotFoundException {
+    	new SportIdNotFoundException(String.valueOf(sportId));
+    }
+    private void validateField(String field) throws SportFieldInvalidException {
+        if (field == null || field.isEmpty()) {
+            throw new SportFieldInvalidException(field);
+        }
+    }
 }
