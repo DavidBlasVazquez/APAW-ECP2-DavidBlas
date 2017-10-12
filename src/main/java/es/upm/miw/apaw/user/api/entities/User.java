e6a41fd1520f7aa66537ad93f4b8dbcb32e303f8
David Blas Vázquez <david.bv@gmail.com>
2017-10-12 18:16:50 +0200

    Fixed error.

--- src/main/java/es/upm/miw/apaw/user/api/entities/User.java
+++ src/main/java/es/upm/miw/apaw/user/api/entities/User.java
@@ -1,14 +1,16 @@
 package es.upm.miw.apaw.user.api.entities;
 
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User  {
 	private Long id;
 	private String name;
 	private Calendar birthDate;
 	private boolean active;
-	private Stack<Sport> sports = new Stack<Sport>();
+	private Map<Long,Sport> sports = new HashMap<Long,Sport>();
 	public User (Long id)  {
 		assert id != null;
 		this.id = id;
@@ -43,10 +45,10 @@ public class User  {
 	}
 	public void addSport(Sport sport) {
 		assert sport != null;
-		sports.add(sport);
+		sports.put(sport.getId(), sport);
 	}
-	public Sport getSport() {
-		if (sports.size() == 0) return null;
-        return sports.pop();
+	public Sport getSport(Long id) {
+		assert id != null;
+		return sports.get(id);
 	}
 }
