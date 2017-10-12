bf19314f7a8fcd54fd92e6e8ec319da79b185ca4
David Blas Vázquez <david.bv@gmail.com>
2017-10-12 05:53:52 +0200

    Changes in Sport Exceptions.

--- src/main/java/es/upm/miw/user/theme/api/resources/exceptions/SportFieldInvalidException.java
+++ src/main/java/es/upm/miw/user/theme/api/resources/exceptions/SportFieldInvalidException.java
@@ -1,14 +1,14 @@
 package es.upm.miw.user.theme.api.resources.exceptions;
 
-public class SportInvalidException extends Exception {
+public class SportFieldInvalidException extends Exception {
 	private static final long serialVersionUID = -6843007179373474952L;
 	public static final String DESCRIPTION = "Id del sport no localizado";
 
-    public SportInvalidException(String detail) {
+    public SportFieldInvalidException(String detail) {
         super(DESCRIPTION + ". " + detail);
     }
 
-    public SportInvalidException() {
+    public SportFieldInvalidException() {
         this("");
     }
 
