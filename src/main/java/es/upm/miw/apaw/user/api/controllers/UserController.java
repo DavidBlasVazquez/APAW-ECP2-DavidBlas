3be9119cf25ff41af223faeae125d49feb506412
David Blas Vázquez <david.bv@gmail.com>
2017-10-13 13:55:07 +0200

    Added to modifyActive and addSportToUser to UseController.

--- src/main/java/es/upm/miw/apaw/user/api/controllers/UserController.java
+++ src/main/java/es/upm/miw/apaw/user/api/controllers/UserController.java
@@ -3,12 +3,19 @@ package es.upm.miw.apaw.user.api.controllers;
 import java.util.Optional;
 
 import es.upm.miw.apaw.user.api.daos.DaoFactory;
+import es.upm.miw.apaw.user.api.dtos.ListSportFromUserDto;
 import es.upm.miw.apaw.user.api.dtos.UserDto;
+import es.upm.miw.apaw.user.api.entities.Sport;
+import es.upm.miw.apaw.user.api.entities.User;
 import es.upm.miw.apaw.user.api.entities.builder.UserBuilder;
 
 public class UserController {
     private boolean existUserId(long userId) {
         return DaoFactory.getFactory().getUserDao().read(userId) != null;
+    }
+    
+    private boolean existSportId(long sportId) {
+        return DaoFactory.getFactory().getSportDao().read(sportId) != null;
     }
     
 	public void createUser(String username) {
@@ -18,6 +25,27 @@ public class UserController {
 	public Optional<UserDto> readUser(long  userId) {
 		if (existUserId(userId)) {
 			return Optional.of(new UserDto(DaoFactory.getFactory().getUserDao().read(userId)));
+		} else {
+			return Optional.empty();
+		}
+	}
+	
+	public Optional<UserDto> modifyActive(Long userId, boolean active) {
+		if (existUserId(userId)) {
+			User user = DaoFactory.getFactory().getUserDao().read(userId);
+			user.setActive(active);
+			return Optional.of(new UserDto(user));
+		} else {
+			return Optional.empty();
+		}
+	}
+
+	public Optional<ListSportFromUserDto> addSportToUser(Long userId, Long sportId) {
+		if (existUserId(userId) && existSportId(sportId)) {
+			User user = DaoFactory.getFactory().getUserDao().read(userId);
+			Sport sport = DaoFactory.getFactory().getSportDao().read(sportId);
+			user.addSport(sport);
+			return Optional.of(new ListSportFromUserDto(user));
 		} else {
 			return Optional.empty();
 		}
