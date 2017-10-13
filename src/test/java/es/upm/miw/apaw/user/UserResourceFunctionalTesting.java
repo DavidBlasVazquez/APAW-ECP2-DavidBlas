f35f09e2bd05928469674c90a59018f0e70d9a59
David Blas Vázquez <david.bv@gmail.com>
2017-10-13 13:57:18 +0200

    Updated User functionality testing to the new changes.

--- src/test/java/es/upm/miw/apaw/user/UserResourceFunctionalTesting.java
+++ src/test/java/es/upm/miw/apaw/user/UserResourceFunctionalTesting.java
@@ -16,17 +16,21 @@ import es.upm.miw.apaw.user.http.HttpRequest;
 import es.upm.miw.apaw.user.http.HttpRequestBuilder;
 
 public class UserResourceFunctionalTesting {
+	private void createUser () {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(UserResource.USER).body("David").build();
+        new HttpClientService().httpRequest(request);
+	}
+	
+	private void createSport () {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(SportResource.SPORT).body("tennis:junior").build();
+        new HttpClientService().httpRequest(request);
+	}
 	
     @Before
     public void before() {
         DaoFactory.setFactory(new DaoMemoryFactory());
     }
     
-	private void createUser () {
-        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(UserResource.USER).body("David").build();
-        new HttpClientService().httpRequest(request);
-	}
-	    
 	@Test
     public void testCreateUSer () {
 		createUser();
@@ -43,12 +47,6 @@ public class UserResourceFunctionalTesting {
         HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path(UserResource.USER).path(UserResource.ID).expandPath("1").build();
         assertEquals("{\"id\":1,\"name\":\"David\",\"active\":\"true\"}", new HttpClientService().httpRequest(request).getBody());        
     }
-	
-    
-    
-    
-    
-    
     
     @Test(expected = HttpException.class)
     public void testIdNotFoundReadUser () {
@@ -56,20 +54,20 @@ public class UserResourceFunctionalTesting {
         HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path(UserResource.USER).path(UserResource.ID).expandPath("2").build();
         new HttpClientService().httpRequest(request).getBody();      
     }
-    /*
+
+	@Test
+    public void testModifyActiveOfUser () {
+		createUser(); // Users are created having the active field as true by default.
+		HttpRequest request = new HttpRequestBuilder().method(HttpMethod.PATCH).path(UserResource.USER).path(UserResource.ID).expandPath("1").path(UserResource.ACTIVE).body("false").build();
+        assertEquals("{\"id:\"1,\"name\":\"David\",\"active\":\"false\"}", new HttpClientService().httpRequest(request).getBody());        	
+    }
+	
     @Test
     public void testAddSportToUser () {
     	createUser();
     	createSport();
         HttpRequest request = new HttpRequestBuilder().method(HttpMethod.PUT).path(UserResource.USER).path(UserResource.ID).expandPath("1").path(UserResource.SPORT).body("1").build();
         String response = new HttpClientService().httpRequest(request).getBody();  
-        assertEquals("{\"id\":1,\"username\":\"David\",\"active\":\"true\", \"sport\":[ {\"title\":\"tennis\", \"category\":\"junior\"} ]}", response);        
-    }
-    */
-	@Test
-    public void testModifyActiveOfUser () {
-		createUser(); // Users are created having the active field as true by default.
-		HttpRequest request = new HttpRequestBuilder().method(HttpMethod.PATCH).path(UserResource.USER).path(UserResource.ID).expandPath("1").path(UserResource.ACTIVE).body("false").build();
-        assertEquals("{\"id\":1,\"username\":\"David\",\"active\":\"false\"}", new HttpClientService().httpRequest(request).getBody());        	
+        assertEquals("{\"id\":1,\"name\":\"David\",\"active\":\"true\", \"sport\":[{\"title\":\"tennis\", \"category\":\"junior\"}]}", response);        
     }
 }
